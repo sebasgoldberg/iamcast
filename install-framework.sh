58f227a461eeebeada0725f561fadc6e358276e5
cerebro <cerebro@ubuntu.(none)>
2012-12-24 10:28:14 -0800

    Se actualiza el script de instalación.

--- install-framework.sh
+++ install-framework.sh
@@ -5,4 +5,9 @@ apt-get install mysql-server
 apt-get install libapache2-mod-wsgi
 apt-get install python-mysqldb
 apt-get install python-imaging
+# El paquete a continuación es necesario para interactuar con base de datos Microsoft SQL Server
 apt-get install python-pymssql 
+apt-get install python-pip
+pip install Django
+echo 'Por favor, debe crear la base de datos y usuario según ha definido en alternativa/settings.py.'
+echo 'Luego debe ejecutar el script reiniciar.sh'
