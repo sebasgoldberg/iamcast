50998c0476c68d76324e39a203d3023a3946f31e
sebasgoldberg <sebas.goldberg@gmail.com>
2012-12-24 08:57:04 -0800

    Script para instalar framework

---
+++ install-framework.sh
@@ -0,0 +1,8 @@
+apt-get install apache2
+apt-get install mysql-server
+# @todo Verificar si es necesario instalar cython
+#apt-get install cython
+apt-get install libapache2-mod-wsgi
+apt-get install python-mysqldb
+apt-get install python-imaging
+apt-get install python-pymssql 
