7dfa6921c71c380ee8dd36d39edec7b7b84df9d6
cerebro <cerebro@ubuntu.(none)>
2013-01-14 09:48:09 -0800

    Instalación: Se agrega instalación de noip y su configuración.

--- install-framework.sh
+++ install-framework.sh
@@ -51,6 +51,31 @@ pip install Django
 mkdir -p uploads/agenciados/fotos
 chmod 777 -R uploads
 
+INSTALL_SCRIPT_DIR=$(pwd)
+
+# Instalacion de no-ip
+which noip2 > /dev/null
+if [ $? -ne 0 ]
+then
+  cd /usr/local/src/
+  wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
+  tar xf noip-duc-linux.tar.gz
+  cd noip-2.1.9-1/
+  make install
+
+# Se configura como servicio y en el startup
+  if [ -f noip2 ]
+  then
+    cp noip2 /etc/init.d/
+    update-rc.d noip2 defaults 90
+  else
+    echo 'ERROR: No se ha encontrado script para servicio noip2'
+  fi
+
+fi
+noip2
+
+
 echo 'A continuación debería realizar las siguientes tareas:'
 echo '1) Crear la base de datos y usuario según ha definido en alternativa/settings.py.'
 echo '2) Ejecutar el script reiniciar.sh'
