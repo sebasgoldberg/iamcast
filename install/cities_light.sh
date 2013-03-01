dc4ba2c9789bff3844227ab6cfa3be6b50f0bf01
Sebastian <sebas.goldberg@gmail.com>
2013-03-01 06:39:12 -0800

    Separación de carga de datos de instalación

--- install/cities_light.sh
+++ install/cities_light.sh
@@ -1,11 +1,2 @@
 easy_install South
 pip install django-cities-light
-echo "A continuación se importarán paises, estados y ciudades."
-echo "Esta tarea puede demorar bastante tiempo."
-echo "Los estados y ciudades a importar serán filtrados en caso de existir alguna señal."
-echo "En caso de no existir ninguna señal serán importados todos los datos."
-./manage.py syncdb
-./manage.py migrate
-./manage.py cities_light
-./manage.py cities_light --force-import BR
-./manage.py cities_light --force-import AR
