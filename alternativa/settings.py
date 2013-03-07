4da9cede83fc3ef9ef8caeefc93fc692399aae45
Sebastian <sebas.goldberg@gmail.com>
2013-03-07 14:10:38 -0800

    Para redefinir un template existente, es necesario hacerlo en templates del proyecto.

--- alternativa/settings.py
+++ alternativa/settings.py
@@ -61,7 +61,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ambiente.project_directory+'uploads/'
+MEDIA_ROOT = ambiente.project_directory+'uploads'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
