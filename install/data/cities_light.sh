7104f40a83fee996cd03097830b90c163c2038aa
sebasgoldberg <sebas.goldberg@gmail.com>
2014-01-08 08:13:31 -0800

    Se quita carpeta install.

--- install/data/cities_light.sh
+++
@@ -1,9 +0,0 @@
-echo "A continuación se importarán paises, estados y ciudades."
-echo "Esta tarea puede demorar bastante tiempo."
-echo "Los estados y ciudades a importar serán filtrados en caso de existir alguna señal."
-echo "En caso de no existir ninguna señal serán importados todos los datos."
-./manage.py syncdb
-./manage.py migrate
-./manage.py cities_light
-#./manage.py cities_light --force-import BR
-#./manage.py cities_light --force-import AR
--- install/smart_selects.sh
+++
@@ -1,3 +0,0 @@
-pip install django-smart-selects
-
-exit $?
