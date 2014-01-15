c0a83c7f0960b5db7c5838f131b5c0c52f2212b2
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-01-15 09:38:49 -0800

    Se muestran los eventos del día y próximos eventos en el dashboard.

--- alternativa/dashboard.py
+++ alternativa/dashboard.py
@@ -14,13 +14,80 @@ from grappelli.dashboard import modules, Dashboard
 from grappelli.dashboard.utils import get_admin_site_name
 from django.conf import settings
 
-from iampacks.agencia.trabajo.models import Trabajo
+from iampacks.agencia.trabajo.models import Trabajo, EventoTrabajo, EventoRol
+
+from datetime import datetime, date, timedelta
 
 
 class CustomIndexDashboard(Dashboard):
     """
     Custom index dashboard for www.
     """
+    def add_eventos_del_dia(self):
+
+      today_date=date.today()
+
+      eventos_dia_trabajo=list(
+        EventoTrabajo.objects.filter(
+          fecha__year=today_date.year,
+          fecha__month=today_date.month,
+          fecha__day=today_date.day).order_by('fecha')
+        )
+
+      eventos_dia_rol=list(
+        EventoRol.objects.filter(
+          fecha__year=today_date.year,
+          fecha__month=today_date.month,
+          fecha__day=today_date.day).order_by('fecha')
+        )
+
+      eventos_dia=eventos_dia_trabajo+eventos_dia_rol
+
+      eventos_dia.sort(key=lambda x: x.fecha)
+
+      links=tuple([
+        ['%s - %s'%(e, e.get_object()), e.get_object().admin_url()] for e in eventos_dia
+        ])
+
+      self.children.append(modules.LinkList(
+        _('Eventos del Dia'),
+        column=3,
+        children=links
+      ))
+
+      return links
+
+    def add_proximos_eventos(self):
+
+      tomorow_date=date.today() + timedelta(days=1)
+      tomorow_datetime=datetime(tomorow_date.year,tomorow_date.month,tomorow_date.day)
+      week_date=date.today() + timedelta(days=6)
+      week_datetime=datetime(week_date.year,week_date.month,week_date.day)
+
+      proximos_eventos_trabajo=list(
+        EventoTrabajo.objects.filter(fecha__gte=tomorow_datetime,fecha__lte=week_datetime)
+        )
+
+      proximos_eventos_rol=list(
+        EventoRol.objects.filter(fecha__gte=tomorow_datetime,fecha__lte=week_datetime)
+        )
+
+      proximos_eventos = proximos_eventos_trabajo + proximos_eventos_rol
+
+      proximos_eventos.sort(key=lambda x: x.fecha)
+
+      links=tuple([
+        ['%s - %s'%(e, e.get_object()), e.get_object().admin_url()] for e in proximos_eventos
+        ])
+
+      self.children.append(modules.LinkList(
+        _('Proximos Eventos'),
+        column=3,
+        children=links
+      ))
+
+      return links
+
     def add_ultimos_trabajos(self,cantidad_trabajos):
 
       ultimos_trabajos=tuple([
@@ -91,6 +158,9 @@ class CustomIndexDashboard(Dashboard):
             column=2,
         ))
 
+        self.add_eventos_del_dia()
+        self.add_proximos_eventos()
+
         # append another link list module for "support".
         self.children.append(modules.LinkList(
             _('Links'),
@@ -114,9 +184,3 @@ class CustomIndexDashboard(Dashboard):
             ]
         ))
 
-        self.children.append(modules.Feed(
-            title=_('Ultimas novedades de la aplicacion'),
-            feed_url='https://github.com/sebasgoldberg/agencia/commits/master.atom',
-            column=3,
-            limit=5,
-        ))
