d42b987fac586f268d7167f606fb87a896723779
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-01-21 05:41:24 -0800

    Actualización: Se incorpora envío de mail a agenciados. Se completan traducciones en iampacks.agencia.

--- update.sh
+++ update.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-pip install django-form-admin==0.4.2
+WD="$(pwd)"
+
+cd ../iampacks/agencia
+git pull origin master
+
+cd ../cross
+git pull origin master
+
+cd "$WD"
+./manage.py syncdb --all
+./manage.py migrate
+echo "Tareas pendientes:"
+echo "- Agregar permiso de envío de mail a agenciados, a grupo de agenciadores."
+
