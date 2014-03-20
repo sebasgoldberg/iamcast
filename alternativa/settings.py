fcd7a309789e2c4e1685532b8712c83990442701
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-03-20 04:13:49 -0700

    Se agrega el middleware de agencia para orientar al usuario en cada paso de configuración.

--- alternativa/settings.py
+++ alternativa/settings.py
@@ -111,6 +111,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'iampacks.agencia.agencia.middleware.AgenciaMiddleware',
 )
 
 ROOT_URLCONF = 'alternativa.urls'
